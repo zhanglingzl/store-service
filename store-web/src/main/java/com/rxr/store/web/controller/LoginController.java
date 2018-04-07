1883763739e2faea16d5bbc31f44ffaa98637476
zhanglingzl <491317077@qq.com>
2018-04-07 14:38:20 +0800

    增加同源限制

--- store-web/src/main/java/com/rxr/store/web/controller/LoginController.java
+++ store-web/src/main/java/com/rxr/store/web/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -15,12 +16,13 @@ import java.util.Map;
 @RestController
 public class LoginController {
 
-    @PostMapping("/ajaxLogin")
+    @RequestMapping("/ajaxLogin")
     public String login(String userName, String password){
-        Map<String,Object> result = new HashMap<String,Object>();
+        Map<String,Object> result = new HashMap<>(10);
         Subject subject = SecurityUtils.getSubject();
         UsernamePasswordToken token = new UsernamePasswordToken(userName, password);
-        if(!subject.isAuthenticated()){ // 判断当前用户是否登录，未登录执行认证
+        // 判断当前用户是否登录，未登录执行认证
+        if(!subject.isAuthenticated()){
             try {
                 subject.login(token);
                 result.put("token", subject.getSession().getId());
